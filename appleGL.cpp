1f17de185c974338badf3ca8306a4aa4f51dbb7e
Jacob <41162160+JacobPox@users.noreply.github.com>
2019-06-14 15:12:07 -0400

    Add files via upload

--- appleGL.cpp
+++ appleGL.cpp
@@ -1,6 +1,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <iostream>
+#include <GL/gl.h>
 
 //Compile with:
 // g++ appleGL.cpp -lglfw -lopengl32
@@ -9,9 +10,23 @@ void famebuffer_size_callback(GLFWwindow *window, int width, int height);
 int createWindow();
 void processInput(GLFWwindow *window);
 
-//Window settings
+//Settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+const char *vertexShaderSource = "#version 330 core\n" //C code!!! :)
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, .5, .2f, 1f);\n"
+    "}\n\0";
 
 void processInput(GLFWwindow *window) {
     //Input control
@@ -69,6 +84,18 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    float triangle[] = {
+        -.5f, .5f, .0f,
+        .5f, -.5f, .0f,
+        .0f, .5f, .0f
+    };
+
+    unsigned int VBO; /*Vertex Buffer Object */
+    glGenBuffers(1, &VBO); //Buffer type
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); //Buffer type for vertex objects are GL_ARRAY_BUFFER
+    //Type of data to buffer, size of data (bytes), data to buffer, and how to manage the data (for graphics driver)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW); /*Position of triangle won't change, so GL_STATIC_DRAW is appropriate */
 
     createWindow();
     
