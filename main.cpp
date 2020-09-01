0157326c3f216e460d51324ab0f890d71adca164
Poxxy <41162160+Poxxy@users.noreply.github.com>
2020-09-01 07:50:23 -0400

    Add a buffer for the triangle.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/* Intended for educational purposes. Read the docs for more help: http://docs.gl/
+   Remember: OpenGL is like a state machine!
+*/
+
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <GLFW/glfw3.h>
@@ -15,7 +19,7 @@ int main(void)
 
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(400, 400, "Graphics Pad", NULL, NULL);
+    window = glfwCreateWindow(400, 400, "Graphics Pad", NULL, NULL); // Width, Height, Title
     if (!window)
     {
         glfwTerminate();
@@ -29,7 +33,23 @@ int main(void)
     /* glewInit() MUST be called after an OpenGL window context has been made. */
     if (glewInit() != GLEW_OK) {
         std::cout << "An Error Has Occured with glewInit()" << std::endl;
+    } else {
+        fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
     }
+
+    /* Triangle */
+    float tri_positions[6] =
+        {
+        -.5f, -.5f,
+        .0f, .5f,
+        .5f, -.5f
+        };
+
+    /* Creates a buffer, useful for things which are static (such as background). */
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), tri_positions, GL_STATIC_DRAW); // 6 float values from 3 vertices (each with 2 coordinates)
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -37,11 +57,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-.5f, -.5f);
-        glVertex2f(.0f, .5f);
-        glVertex2f(.5f, -.5f);
-        glEnd();
+        /* Calls upon buffer */
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
